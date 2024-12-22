f0baa5b0fe317fc0d00e2332666e2a0cd2ffbb3f
spark-o-petit <petitspark247@gmail.com>
2024-12-23 02:24:11 +0900

    Declared first variable

---
+++ MyFirstApp/Variable.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by alex on 12/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Alex!")
+            Text("Hello, Alex!")
+            Text("Hello, Alex!")
+          
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
